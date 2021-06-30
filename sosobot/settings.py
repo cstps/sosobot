ad4df97700c5b274d56e39a86dc2ba3fb0ade873
cstps <seotos@g.gne.go.kr>
2021-06-30 20:30:45 +0900

    navbar 추가, login 기능 추가

--- sosobot/settings.py
+++ sosobot/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -123,6 +123,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+# 로그인 성공후 이동하는 URL
+LOGIN_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- sosobot/views.py
+++ sosobot/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    item = {x for x in range(20)}
+    item = {x:x**x for x in range(1,20)}
     content = {'content':item}
         
     return render(request, 'index.html',content)
